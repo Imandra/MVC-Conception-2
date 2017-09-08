9ed9cc5961c393ccf3dedc7ccedc81c58b5eac1d
Imandra <shalyapin.alexej@yandex.ru>
2017-09-08 21:29:56 +0400

    mod_rewrite

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
 require_once __DIR__ . '/autoload.php';
-
+//$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+//$pathParts = explode('/', $path);
+//$ctrl = !empty($pathParts[1]) ? ucfirst($pathParts[1]) : 'News';
+//$act = !empty($pathParts[2]) ? ucfirst($pathParts[2]) : 'All';
+// в .htaccess: RewriteRule ^(.*)$ index.php
 $ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
 $act = isset($_GET['act']) ? $_GET['act'] : 'All';
 
